57856be8b17369bfb327da1bdd548c6adfe9fbe6
lukasvici <izi98izi2@gmail.com>
2022-05-09 12:48:27 +0300

    del comments, and using`s

--- ZooApp/Pages/AddEditAnimals.xaml.cs
+++ ZooApp/Pages/AddEditAnimals.xaml.cs
@@ -1,25 +1,14 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZooApp.Pages
 {
-    /// <summary>
-    /// Interaction logic for AddEditAnimals.xaml
-    /// </summary>
     public partial class AddEditAnimals : Page
     {
         private Animal curAnimal = new Animal();
--- ZooApp/Pages/MainPage.xaml.cs
+++ ZooApp/Pages/MainPage.xaml.cs
@@ -1,23 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ZooApp.Pages
 {
-    /// <summary>
-    /// Interaction logic for MainPage.xaml
-    /// </summary>
     public partial class MainPage : Page
     {
         string[] PermissionPages = { "Животные", "Расписание", "Сотрудники" };
